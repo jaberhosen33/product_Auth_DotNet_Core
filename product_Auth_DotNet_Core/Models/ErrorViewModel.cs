e4b38996cbd5087e41bcd3f3225f96be1c98d9d7
jaberhosen33 <jaberhosen33@gmail.com>
2025-01-25 19:32:18 +0600

    Add project files.

---
+++ product_Auth_DotNet_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace product_Auth_DotNet_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ product_Auth_DotNet_Core/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace product_Auth_DotNet_Core.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Role { get; set; } = "User";
+    }
+}
